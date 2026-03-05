938bd391e2914b092380ad57343f4897d9c44350
Yoonisabdulqaadir <yoonisabdikhaadir@gmail.com>
2026-03-05 12:05:56 +0300

    adding the exercise 5

---
+++ Exercise 5/main.js
@@ -0,0 +1,7 @@
+const multiplyValues = (x ,y)=> {
+    return Number(x) * Number(y);
+};
+console.log("Arrow Function:");
+console.log(multiplyValues(4, 6));      
+console.log(multiplyValues(3, 7));  
+console.log(multiplyValues(10, 11)); 
