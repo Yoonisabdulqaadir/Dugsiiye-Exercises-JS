d47b23a2ffef16de4dbc6e6d87de6c3821130a45
Yoonisabdulqaadir <yoonisabdikhaadir@gmail.com>
2026-03-08 16:30:12 +0300

    adding

---
+++ Exercise 7/main.js
@@ -0,0 +1,8 @@
+let colors= ['red','green','blue'];
+
+console.log('before adding');
+colors.push('yellow');
+console.log(colors);
+console.log('before removing front');
+colors.shift();
+console.log(colors);
---
+++ Exercise 8/main.js
@@ -0,0 +1,15 @@
+let cars = {
+    make : 2004,
+    model : 'GMC',
+    year :  2018
+};
+console.log(cars.make);
+console.log(cars.model);
+console.log(cars.year);
+
+
+// way of array 
+
+console.log(cars["make"]);
+console.log(cars["model"]);
+console.log(cars["year"]);
